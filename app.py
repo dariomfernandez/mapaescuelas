1ae61e185a909026b21e8cc7189b85d320af1a2a
dariomfernandez <74682808+dariomfernandez@users.noreply.github.com>
2022-09-28 21:36:19 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
 
+folder = ""
+
+df = pd.read_excel(folder + 'escuelas-coordenadas-agosto-2018.xlsx')
+
+st.header('Escuelas')
+
+df = df.query("departamento == 'SAN RAFAEL'")
+df = df[['lat','lon']]
+#print(df.head())
+st.map(df)
